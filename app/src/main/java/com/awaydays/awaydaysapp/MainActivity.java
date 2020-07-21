08c819e1cbbce798597ebbfdb80386c1e6feaa3b
DominikVolz <40237926+DominikVolz@users.noreply.github.com>
2020-07-21 16:28:45 +0200

    Hallo Herr Rößler

--- app/src/main/java/com/awaydays/awaydaysapp/MainActivity.java
+++ app/src/main/java/com/awaydays/awaydaysapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.awaydays.awaydaysapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }

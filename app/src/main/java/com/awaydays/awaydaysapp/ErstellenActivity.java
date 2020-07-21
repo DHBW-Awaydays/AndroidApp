0074cbc814f4da65c9f9a3736dc4e85f522e0c54
DominikVolz <40237926+DominikVolz@users.noreply.github.com>
2020-07-21 23:43:38 +0200

    Fahrterstellen

---
+++ app/src/main/java/com/awaydays/awaydaysapp/ErstellenActivity.java
@@ -0,0 +1,22 @@
+package com.awaydays.awaydaysapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ErstellenActivity extends AppCompatActivity {
+    ArrayList<Fahrt> mData;
+    MyAdapter mMyAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erstellen);
+
+
+    }
+
+}

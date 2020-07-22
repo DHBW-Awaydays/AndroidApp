6220ff6782ff4bbd109ab7bf18b92d1ae0630374
EricRissler <eric.rissler@gmx.de>
2020-07-22 13:37:29 +0200

    Such, detail und erstellen Activity modifiziert

--- app/src/main/java/com/awaydays/awaydaysapp/detail.java
+++ app/src/main/java/com/awaydays/awaydaysapp/detail.java
@@ -1,20 +1,56 @@
 package com.awaydays.awaydaysapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class detail extends AppCompatActivity {
     Toolbar toolbar;
+
+    int counter =0;
+
+    String amount ="0";
+
+    TextView cView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        cView=findViewById(R.id.anzahlPlätzeTv);
 
     }
 
+    public void plus (View view)
+    {
+        if(counter<13)
+        {
+            counter++;
 
+        }
+
+        amount=Integer.toString(counter);
+
+        cView.setText(amount);
+    }
+
+    public void minus (View view)
+    {
+        if(counter>0)
+        {
+            counter--;
+        }
+
+        amount=Integer.toString(counter);
+
+        cView.setText(amount);
+
+
+
+    }
 
 }
--- app/src/main/java/com/awaydays/awaydaysapp/search.java
+++ app/src/main/java/com/awaydays/awaydaysapp/search.java
@@ -1,44 +1,63 @@
 package com.awaydays.awaydaysapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class search extends AppCompatActivity {
+    Toolbar toolbar;
 
-    Intent intentDashboard;
-    Intent intentPlus;
+    int counter=0;
+
+    String amount= "0";
+
+    TextView cView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        ImageButton plusBtn = (ImageButton) findViewById(R.id.plusBtn);
-        TextView awaydaysTv = (TextView) findViewById(R.id.awaydaysTv);
+        cView= findViewById(R.id.anzahlPlätzeTv);
 
-        intentPlus = new Intent(this, erstellen.class);
-        plusBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intentPlus);
-            }
-        });
 
-        intentDashboard = new Intent(this, MainActivity.class);
-        awaydaysTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intentDashboard);
-            }
-        });
+
 
     }
+
+    public void plus (View view)
+    {
+        if(counter<13)
+        {
+            counter++;
+
+        }
+
+        amount=Integer.toString(counter);
+
+        cView.setText(amount);
+    }
+
+    public void minus (View view)
+    {
+        if(counter>0)
+        {
+            counter--;
+        }
+
+        amount=Integer.toString(counter);
+
+        cView.setText(amount);
+
+
+
+    }
+
 
 
 

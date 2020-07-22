c9dffcb21f45daa9606578ce4ff4e8532060ff68
EricRissler <eric.rissler@gmx.de>
2020-07-22 10:23:49 +0200

    modified gitignore

--- app/src/main/java/com/awaydays/awaydaysapp/MainActivity.java
+++ app/src/main/java/com/awaydays/awaydaysapp/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.View;
-import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/awaydays/awaydaysapp/erstellen.java
+++ app/src/main/java/com/awaydays/awaydaysapp/erstellen.java
@@ -3,8 +3,6 @@ package com.awaydays.awaydaysapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 

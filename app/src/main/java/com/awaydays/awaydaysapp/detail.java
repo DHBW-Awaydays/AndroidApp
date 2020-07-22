e804de4f087b0c704cb17184b6edbd951124ee43
EricRissler <eric.rissler@gmx.de>
2020-07-22 09:41:59 +0200

    modified gitignore

---
+++ app/src/main/java/com/awaydays/awaydaysapp/detail.java
@@ -0,0 +1,20 @@
+package com.awaydays.awaydaysapp;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class detail extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+    }
+
+
+
+}

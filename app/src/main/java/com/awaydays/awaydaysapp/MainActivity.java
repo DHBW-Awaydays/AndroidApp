9bae2efae561e87f9d8fdff43e3a46878de21d70
DominikVolz <40237926+DominikVolz@users.noreply.github.com>
2020-07-22 10:43:29 +0200

    Intent Dashboard

--- app/src/main/java/com/awaydays/awaydaysapp/MainActivity.java
+++ app/src/main/java/com/awaydays/awaydaysapp/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Fahrt> mData;
     MyAdapter mMyAdapter;
+    Intent intentPlus;
+    Intent intentLupe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +40,26 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter((mMyAdapter));
 
+        ImageButton searchBtn = (ImageButton) findViewById(R.id.searchBtn);
+        ImageButton plusBtn = (ImageButton) findViewById(R.id.plusBtn);
+
+        intentLupe = new Intent(this, search.class);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentLupe);
+            }
+        });
+
+        intentPlus = new Intent(this, erstellen.class);
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentPlus);
+            }
+        });
     }
+
     public void clickPlus(){
 
     }
